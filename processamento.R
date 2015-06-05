a876bb7604c578661f281041d7e7bc766ab84366
Luciano Pacheco <lucianopacheco@gmail.com>
2015-06-05 18:52:42 -0300

    ultimo

--- processamento.R
+++ processamento.R
@@ -1,20 +1,31 @@
+library(data.table)
+library(dplyr)
 
-remun = read.table("20150331_Remuneracao.csv", sep = "\t", header = TRUE)
-cad = read.table("20150331_Cadastro.csv", sep = "\t", header = TRUE, fill = TRUE)
+
+remun = read.table("20150331_Remuneracao.csv", sep = "\t", header = TRUE, fill = TRUE,  quote = "", dec = ",")
+cad = read.table("20150331_Cadastro.csv", sep = "\t", header = TRUE, fill = TRUE,  quote = "")
 cad = cad %>% select(Id_SERVIDOR_PORTAL, NOME, DESCRICAO_CARGO, FUNCAO, ORG_LOTACAO, ORGSUP_LOTACAO, ORG_EXERCICIO, ORGSUP_EXERCICIO, SITUACAO_VINCULO, REGIME_JURIDICO, UF_EXERCICIO)
 remun = remun %>% select(ID_SERVIDOR_PORTAL, REMUNERAÇÃO.BÁSICA.BRUTA..R.., REMUNERAÇÃO.APÓS.DEDUÇÕES.OBRIGATÓRIAS..R.., TOTAL.DE.VERBAS.INDENIZATÓRIAS..R....., TOTAL.DE.HONORÁRIOS..JETONS. )
 names(remun) = c("ID", "REMUNERACAO_BRUTA", "REMUNERACAO_LIQUIDA", "VERBAS", "JETONS")
 cad = cad %>% rename(ID = Id_SERVIDOR_PORTAL)
 cad$ID = as.integer(as.character(cad$ID))
 
-desc_cargo= servidores %>% select(ID, DESCRICAO_CARGO)
-funcao = servidores %>% select(ID, FUNCAO)
-funcao = funcao %>% distinct()
-desc_cargo = desc_cargo %>% distinct()
-desc_cargo = desc_cargo %>% filter(DESCRICAO_CARGO != "")
-funcao = funcao %>% filter(FUNCAO != "")
+desc_cargo= cad %>% select(ID, DESCRICAO_CARGO)
+funcao = cad %>% select(ID, FUNCAO)
+funcao = funcao %>% distinct() %>% filter(FUNCAO != "")
+desc_cargo = desc_cargo %>% distinct() %>% filter(DESCRICAO_CARGO != "")
 
-servidores = servidores %>% select(-DESCRICAO_CARGO, -FUNCAO)
 
+servidores = cad %>% select(-DESCRICAO_CARGO, -FUNCAO)
 servidores = servidores %>% distinct()
-servidores = left_join(servidores, cargos, by="ID")
+servidores = left_join(servidores, desc_cargo, by="ID")
+servidores = left_join(servidores, funcao, by="ID")
+servidores = left_join(remun, servidores, by="ID")
+servidores$REMUNERACAO_BRUTA = as.numeric(servidores$REMUNERACAO_BRUTA)
+servidores$REMUNERACAO_LIQUIDA = as.numeric(servidores$REMUNERACAO_LIQUIDA)
+servidores$VERBAS = as.numeric(servidores$VERBAS)
+servidores$JETONS = as.numeric(servidores$JETONS)
+
+
+
+saveRDS(servidores, file = "servidores.rds")
--- server.R
+++ server.R
@@ -11,34 +11,34 @@ shinyServer( function(input, output, session) {
   # servidores = read.table("servidores2.csv", header = TRUE, sep = ";")
   servidores <- readRDS("servidores.rds")
   servidores$NOME = as.character(servidores$NOME)
-  carreiras_populosas = servidores %>% select(CARGO) %>% group_by(CARGO) %>% summarise(n=n()) %>% 
-                arrange(desc(n)) %>% slice(1:20) %>% select(CARGO) %>% as.matrix() %>% c()
-  carreiras_bempagas = servidores %>% group_by(CARGO) %>% summarise(n=mean(REMUNERACAO)) %>% 
-                arrange(desc(n)) %>% slice(1:20) %>% select(CARGO) %>% as.matrix() %>% c()
+  carreiras_populosas = servidores %>% select(DESCRICAO_CARGO) %>% group_by(DESCRICAO_CARGO) %>% summarise(n=n()) %>% 
+                arrange(desc(n)) %>% slice(1:20) %>% select(DESCRICAO_CARGO) %>% as.matrix() %>% c()
+  carreiras_bempagas = servidores %>% group_by(DESCRICAO_CARGO) %>% summarise(n=mean(REMUNERACAO_BRUTA)) %>% 
+                arrange(desc(n)) %>% slice(1:20) %>% select(DESCRICAO_CARGO) %>% as.matrix() %>% c()
   
   ####################################SUMARIO###########################################    
-  carreiras_grandes = reactive({servidores %>% group_by(CARGO) %>% summarise(N=n()) %>% 
-      filter(N>input$min_servidores) %>% select(CARGO) %>% as.matrix() %>% c()})
-  carreiras_bem_remun = reactive({servidores %>% group_by(CARGO) %>% summarise(N=mean(REMUNERACAO)) %>% 
+  carreiras_grandes = reactive({servidores %>% group_by(DESCRICAO_CARGO) %>% summarise(N=n()) %>% 
+      filter(N>input$min_servidores) %>% select(DESCRICAO_CARGO) %>% as.matrix() %>% c()})
+  carreiras_bem_remun = reactive({servidores %>% group_by(DESCRICAO_CARGO) %>% summarise(N=mean(REMUNERACAO_BRUTA)) %>% 
       filter(N>input$min_remuneracao) %>% as.matrix() %>% c()})
   
   # Histograma
   output$plot1 <- renderPlot({
-    servidores %>% filter(CARGO %in% carreiras_bem_remun(), CARGO %in% carreiras_grandes()) %>% filter(!is.na(UF)) %>% 
-    ggplot(aes(REMUNERACAO)) + 
+    servidores %>% filter(DESCRICAO_CARGO %in% carreiras_bem_remun(), DESCRICAO_CARGO %in% carreiras_grandes()) %>% filter(!is.na(UF_EXERCICIO)) %>% 
+    ggplot(aes(REMUNERACAO_BRUTA)) + 
       geom_histogram(colour = "darkblue", fill = "skyblue") + 
-      geom_vline(aes(xintercept=mean(REMUNERACAO, na.rm=T)), color="red", linetype="dashed", size=0.8) +
-      geom_vline(aes(xintercept=median(REMUNERACAO, na.rm=T)), color="black", linetype="dashed", size=0.8) +
-      labs(x="Remuneração", y="", title="Histograma") +
-      scale_y_continuous(breaks=seq(0,60000,5000)) + scale_x_continuous(breaks=seq(0,50000,5000))
+      geom_vline(aes(xintercept=mean(REMUNERACAO_BRUTA, na.rm=T)), color="red", linetype="dashed", size=0.8) +
+      geom_vline(aes(xintercept=median(REMUNERACAO_BRUTA, na.rm=T)), color="black", linetype="dashed", size=0.8) +
+      labs(x="Remuneração", y="", title="Histograma") 
+      # scale_y_continuous(breaks=seq(0,60000,5000)) + scale_x_continuous(breaks=seq(0,50000,5000))
     
     
   })
   
   # Boxplot
   output$plot2 <- renderPlot({
-    servidores %>% filter(CARGO %in% carreiras_bem_remun(), CARGO %in% carreiras_grandes()) %>%  
-      ggplot(aes(UF, REMUNERACAO)) + 
+    servidores %>% filter(DESCRICAO_CARGO %in% carreiras_bem_remun(), DESCRICAO_CARGO %in% carreiras_grandes()) %>%  
+      ggplot(aes(UF_EXERCICIO, REMUNERACAO_BRUTA)) + 
       geom_boxplot(colour = "black", fill = "#FF9999") + 
       labs(x="Estado", y="", title="Boxplot") +
       scale_y_continuous(breaks=seq(0,60000,3000))
@@ -46,11 +46,11 @@ shinyServer( function(input, output, session) {
   
   # Top carreiras mais populosas
   output$plot7 <- renderPlot({
-    servidores %>% filter(CARGO %in% carreiras_bem_remun(), CARGO %in% carreiras_grandes(), !is.na(CARGO)) %>% 
-      group_by(CARGO) %>% 
-      summarise(N_Servidores = n(), Salario_Medio = mean(REMUNERACAO), Salario_Max = max(REMUNERACAO), Salario_Min = min(REMUNERACAO)) %>% 
+    servidores %>% filter(DESCRICAO_CARGO %in% carreiras_bem_remun(), DESCRICAO_CARGO %in% carreiras_grandes(), !is.na(DESCRICAO_CARGO)) %>% 
+      group_by(DESCRICAO_CARGO) %>% 
+      summarise(N_Servidores = n(), Salario_Medio = mean(REMUNERACAO_BRUTA), Salario_Max = max(REMUNERACAO_BRUTA), Salario_Min = min(REMUNERACAO_BRUTA)) %>% 
       arrange(desc(N_Servidores)) %>% slice(1:15) %>%
-      ggplot(aes(x=reorder(CARGO, Salario_Medio), y=Salario_Medio)) + 
+      ggplot(aes(x=reorder(DESCRICAO_CARGO, Salario_Medio), y=Salario_Medio)) + 
       geom_bar(stat="identity", fill = "darkgreen") + 
       labs(x="", y="Salario Medio", title="As 15 Carreiras mais populosas") +
       coord_flip()
@@ -59,23 +59,23 @@ shinyServer( function(input, output, session) {
   
   # numero de carreiras
   output$text1 <- renderText ({
-    t=servidores %>% filter(CARGO %in% carreiras_bem_remun(), CARGO %in% carreiras_grandes()) %>% 
-      group_by(CARGO) %>% summarise(N_Servidores = n()) %>% summarise(n()) %>% as.character()
+    t=servidores %>% filter(DESCRICAO_CARGO %in% carreiras_bem_remun(), DESCRICAO_CARGO %in% carreiras_grandes()) %>% 
+      group_by(DESCRICAO_CARGO) %>% summarise(N_Servidores = n()) %>% summarise(n()) %>% as.character()
     paste("Número de Carreiras:", t)
   })
   
   # numero de servidores
   output$text2 <- renderText ({
-    t=servidores %>% filter(CARGO %in% carreiras_bem_remun(), CARGO %in% carreiras_grandes()) %>% 
-      group_by(CARGO) %>% summarise(N_Servidores = n()) %>% summarise(sum(N_Servidores)) %>% as.character()
+    t=servidores %>% filter(DESCRICAO_CARGO %in% carreiras_bem_remun(), DESCRICAO_CARGO %in% carreiras_grandes()) %>% 
+      group_by(DESCRICAO_CARGO) %>% summarise(N_Servidores = n()) %>% summarise(sum(N_Servidores)) %>% as.character()
     paste("Número de Servidores:", t)
     
   })
   
   #####################################CARREIRAS##########################################  
   
-  REMUN_TOTAL = sum(servidores$REMUNERACAO)
-  CARGOS = servidores %>% select(CARGO) %>% distinct() %>% arrange(desc(CARGO)) %>% as.matrix() %>% c()
+  REMUN_TOTAL = sum(servidores$REMUNERACAO_BRUTA)
+  CARGOS = servidores %>% select(DESCRICAO_CARGO) %>% distinct() %>% arrange(desc(DESCRICAO_CARGO)) %>% as.matrix() %>% c()
   
   # Sidepanel
   output$carr1 <- renderUI({
@@ -90,7 +90,7 @@ shinyServer( function(input, output, session) {
   
   # TAB Todas
   output$carreiras <- DT::renderDataTable({
-    datatable(servidores %>% group_by(CARGO) %>% summarise(Salario_Medio = round(mean(REMUNERACAO, rm.na=TRUE), 2), N_Servidores = n(), Gasto_Total=sum(REMUNERACAO), Percentual = round((Gasto_Total*100/REMUN_TOTAL),2)) %>% 
+    datatable(servidores %>% group_by(DESCRICAO_CARGO) %>% summarise(Salario_Medio = round(mean(REMUNERACAO_BRUTA, rm.na=TRUE), 2), N_Servidores = n(), Gasto_Total=sum(REMUNERACAO_BRUTA), Percentual = round((Gasto_Total*100/REMUN_TOTAL),2)) %>% 
       arrange(desc(Salario_Medio))) 
   })
   
@@ -103,28 +103,28 @@ shinyServer( function(input, output, session) {
   output$carreiras3 <- DT::renderDataTable({
     if(is.null(input$carr2))
       return()
-    SALARIO_CAR = servidores %>% filter(CARGO==input$carr2) %>% group_by(CARGO) %>%
-                  summarise(Numero_Servidores = n(), Salario_Medio = round(mean(REMUNERACAO), 2), Salario_Maximo = max(REMUNERACAO), Salario_Minimo = min(REMUNERACAO))
+    SALARIO_CAR = servidores %>% filter(DESCRICAO_CARGO==input$carr2) %>% group_by(DESCRICAO_CARGO) %>%
+                  summarise(Numero_Servidores = n(), Salario_Medio = round(mean(REMUNERACAO_BRUTA), 2), Salario_Maximo = max(REMUNERACAO_BRUTA), Salario_Minimo = min(REMUNERACAO_BRUTA))
     datatable(SALARIO_CAR)
   })
   
   output$plot_comp1 <- renderPlot({
-    servidores %>% filter(CARGO %in% input$carr2) %>% 
-      ggplot(aes(REMUNERACAO, fill = CARGO)) + 
+    servidores %>% filter(DESCRICAO_CARGO %in% input$carr2) %>% 
+      ggplot(aes(REMUNERACAO_BRUTA, fill = DESCRICAO_CARGO)) + 
       geom_histogram(alpha=.8, position="dodge") + 
       labs(x="Remuneração", y="", title="Distribuição") 
   })
   
   output$plot_comp2 <- renderPlot({
-    servidores %>% filter(CARGO %in% input$carr2) %>% 
-      ggplot(aes(REMUNERACAO, fill = CARGO)) + 
+    servidores %>% filter(DESCRICAO_CARGO %in% input$carr2) %>% 
+      ggplot(aes(REMUNERACAO_BRUTA, fill = DESCRICAO_CARGO)) + 
       geom_density(alpha=0.2) + 
       labs(x="Remuneração", y="", title="Distribuição") 
   })
   
   output$plot_comp3 <- renderPlot({
-    servidores %>% filter(CARGO %in% input$carr2) %>% group_by(CARGO, UF) %>% summarise(TOTAL=n()) %>%
-      ggplot(aes(UF, TOTAL, fill = CARGO)) + 
+    servidores %>% filter(DESCRICAO_CARGO %in% input$carr2) %>% group_by(DESCRICAO_CARGO, UF_EXERCICIO) %>% summarise(TOTAL=n()) %>%
+      ggplot(aes(UF_EXERCICIO, TOTAL, fill = DESCRICAO_CARGO)) + 
       geom_bar(stat = "identity", alpha=.8, position="dodge") +
       labs(x="Remuneração", y="", title="Servidores por Estado") 
   })
@@ -132,8 +132,8 @@ shinyServer( function(input, output, session) {
   output$plot_comp4 <- renderPlot({
     if(is.null(input$carr2))
       return()
-    servidores %>% filter(CARGO %in% input$carr2) %>% group_by(UF) %>%
-              ggplot(aes(UF, REMUNERACAO, fill = CARGO)) + 
+    servidores %>% filter(DESCRICAO_CARGO %in% input$carr2) %>% group_by(UF_EXERCICIO) %>%
+              ggplot(aes(UF_EXERCICIO, REMUNERACAO_BRUTA, fill = DESCRICAO_CARGO)) + 
               geom_boxplot(colour = "darkblue") + 
               labs(x="Estado", y="Quantidade de Servidores", title="Boxplot")
   })
@@ -148,12 +148,12 @@ shinyServer( function(input, output, session) {
   output$texto = renderText({paste("Nome: ", nome())})
   
   output$tab_resumo <- DT::renderDataTable({
-    serv_remuneracao = servidores %>% filter(NOME %in% nome()) %>% select(REMUNERACAO)
-    serv_cargo = servidores %>% filter(NOME %in% nome()) %>% select(CARGO)
-    remun_cargo = servidores %>% filter(CARGO == serv_cargo[1,]) %>% summarise(Num_Serv = n(), Media = round(mean(REMUNERACAO),2), Min = min(REMUNERACAO), Max = max(REMUNERACAO))
-    num_serv_carg = servidores %>% filter(CARGO == serv_cargo[1,]) %>% summarise(N_Servidores_carreira = n())
-    num_serv_acima = servidores %>% filter(CARGO == serv_cargo[1,] & REMUNERACAO > serv_remuneracao[1,]) %>% summarise(N_Servidores_Acima = n())
-    num_serv_abaixo = servidores %>% filter(CARGO == serv_cargo[1,] & REMUNERACAO < serv_remuneracao[1,]) %>% summarise(N_Servidores_Abaixo = n())
+    serv_remuneracao = servidores %>% filter(NOME %in% nome()) %>% select(REMUNERACAO_BRUTA)
+    serv_cargo = servidores %>% filter(NOME %in% nome()) %>% select(DESCRICAO_CARGO)
+    remun_cargo = servidores %>% filter(DESCRICAO_CARGO == serv_cargo[1,]) %>% summarise(Num_Serv = n(), Media = round(mean(REMUNERACAO_BRUTA),2), Min = min(REMUNERACAO_BRUTA), Max = max(REMUNERACAO_BRUTA))
+    num_serv_carg = servidores %>% filter(DESCRICAO_CARGO == serv_cargo[1,]) %>% summarise(N_Servidores_carreira = n())
+    num_serv_acima = servidores %>% filter(DESCRICAO_CARGO == serv_cargo[1,] & REMUNERACAO_BRUTA > serv_remuneracao[1,]) %>% summarise(N_Servidores_Acima = n())
+    num_serv_abaixo = servidores %>% filter(DESCRICAO_CARGO == serv_cargo[1,] & REMUNERACAO_BRUTA < serv_remuneracao[1,]) %>% summarise(N_Servidores_Abaixo = n())
     resumo_geral = cbind(num_serv_carg, num_serv_acima, num_serv_abaixo, remun_cargo)
     datatable(resumo_geral) 
   })
