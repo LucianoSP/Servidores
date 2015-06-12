5c91ab451689d9306efc8938d71a12dd26a18869
Luciano Pacheco <lucianopacheco@gmail.com>
2015-06-12 18:12:28 -0300

    ultimo

--- server.R
+++ server.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(data.table)
 library(DT)
 library(ggplot2)
+library(ggthemes)
 
 
 shinyServer( function(input, output, session) {
@@ -10,11 +11,12 @@ shinyServer( function(input, output, session) {
   
   # servidores = read.table("servidores2.csv", header = TRUE, sep = ";")
   servidores <- readRDS("servidores2.rds")
+  servidores = servidores %>% filter(SITUACAO_VINCULO != "EXERC DESCENT CARREI", SITUACAO_VINCULO != "REQUISITADO")
   servidores$NOME = as.character(servidores$NOME)
   carreiras_populosas = servidores %>% select(DESCRICAO_CARGO) %>% group_by(DESCRICAO_CARGO) %>% summarise(n=n()) %>% 
-                arrange(desc(n)) %>% slice(1:20) %>% select(DESCRICAO_CARGO) %>% as.matrix() %>% c()
+    arrange(desc(n)) %>% slice(1:20) %>% select(DESCRICAO_CARGO) %>% as.matrix() %>% c()
   carreiras_bempagas = servidores %>% group_by(DESCRICAO_CARGO) %>% summarise(n=mean(REMUNERACAO_BRUTA)) %>% 
-                arrange(desc(n)) %>% slice(1:20) %>% select(DESCRICAO_CARGO) %>% as.matrix() %>% c()
+    arrange(desc(n)) %>% slice(1:20) %>% select(DESCRICAO_CARGO) %>% as.matrix() %>% c()
   
   ####################################SUMARIO###########################################    
   carreiras_grandes = reactive({servidores %>% group_by(DESCRICAO_CARGO) %>% summarise(N=n()) %>% 
@@ -24,58 +26,70 @@ shinyServer( function(input, output, session) {
   
   # Histograma
   output$plot1 <- renderPlot({
-    servidores %>% filter(DESCRICAO_CARGO %in% carreiras_bem_remun(), DESCRICAO_CARGO %in% carreiras_grandes()) %>% filter(!is.na(UF_EXERCICIO)) %>% 
-    ggplot(aes(REMUNERACAO_BRUTA)) + 
+    input$action1
+    isolate(servidores %>% filter(DESCRICAO_CARGO %in% carreiras_bem_remun(), DESCRICAO_CARGO %in% carreiras_grandes()) %>% filter(!is.na(UF_EXERCICIO)) %>% 
+      ggplot(aes(REMUNERACAO_BRUTA)) + 
       geom_histogram(colour = "darkblue", fill = "skyblue") + 
       geom_vline(aes(xintercept=mean(REMUNERACAO_BRUTA, na.rm=T)), color="red", linetype="dashed", size=0.8) +
       geom_vline(aes(xintercept=median(REMUNERACAO_BRUTA, na.rm=T)), color="black", linetype="dashed", size=0.8) +
-      labs(x="Remuneração", y="", title="Histograma") 
-      # scale_y_continuous(breaks=seq(0,60000,5000)) + scale_x_continuous(breaks=seq(0,50000,5000))
+      labs(x="Remuneração", y="", title="Histograma") +
+      #     scale_y_continuous(breaks=seq(0,60000,5000)) + 
+      scale_x_continuous(breaks=seq(0,50000,5000)) + 
+      theme(plot.title = element_text(size=15, face="bold", vjust=2)) 
+      
+      )
     
     
   })
   
   # Boxplot
   output$plot2 <- renderPlot({
-    servidores %>% filter(DESCRICAO_CARGO %in% carreiras_bem_remun(), DESCRICAO_CARGO %in% carreiras_grandes()) %>%  
+    input$action1
+    isolate(servidores %>% filter(DESCRICAO_CARGO %in% carreiras_bem_remun(), DESCRICAO_CARGO %in% carreiras_grandes()) %>%  
       ggplot(aes(UF_EXERCICIO, REMUNERACAO_BRUTA)) + 
       geom_boxplot(colour = "black", fill = "#FF9999") + 
       labs(x="Estado", y="", title="Boxplot") +
-      scale_y_continuous(breaks=seq(0,60000,3000))
+      scale_y_continuous(breaks=seq(0,60000,3000)) + 
+      theme(plot.title = element_text(size=15, face="bold", vjust=2)) +
+      theme_stata() + scale_colour_stata() 
+      )
   })
   
   # Top carreiras mais populosas
   output$plot7 <- renderPlot({
-    servidores %>% filter(DESCRICAO_CARGO %in% carreiras_bem_remun(), DESCRICAO_CARGO %in% carreiras_grandes(), !is.na(DESCRICAO_CARGO)) %>% 
+    input$action1
+    isolate(servidores %>% filter(DESCRICAO_CARGO %in% carreiras_bem_remun(), DESCRICAO_CARGO %in% carreiras_grandes(), !is.na(DESCRICAO_CARGO)) %>% 
       group_by(DESCRICAO_CARGO) %>% 
       summarise(N_Servidores = n(), Salario_Medio = mean(REMUNERACAO_BRUTA)) %>% 
       arrange(desc(N_Servidores)) %>% slice(1:15) %>%
-      ggplot(aes(x=DESCRICAO_CARGO, y=Salario_Medio, size = N_Servidores)) +
-      geom_point(colour="white", fill="red", shape=21) + scale_size_area(max_size = 20) +
+      ggplot(aes(x=DESCRICAO_CARGO, y=Salario_Medio)) +
+      geom_bar(stat = "identity", colour="white", fill="red")  +
       labs(x="", y="Salario Medio", title="As 15 Carreiras mais populosas") +
+      theme(plot.title = element_text(size=15, face="bold", vjust=2)) +
+        
       coord_flip()
-   
+    )
   })
   
   # numero de carreiras
   output$text1 <- renderText ({
     t=servidores %>% filter(DESCRICAO_CARGO %in% carreiras_bem_remun(), DESCRICAO_CARGO %in% carreiras_grandes()) %>% 
       group_by(DESCRICAO_CARGO) %>% summarise(N_Servidores = n()) %>% summarise(n()) %>% as.character()
-    paste("Número de Carreiras:", t)
+    paste("   Número de Carreiras: ", t)
   })
   
   # numero de servidores
   output$text2 <- renderText ({
     t=servidores %>% filter(DESCRICAO_CARGO %in% carreiras_bem_remun(), DESCRICAO_CARGO %in% carreiras_grandes()) %>% 
       group_by(DESCRICAO_CARGO) %>% summarise(N_Servidores = n()) %>% summarise(sum(N_Servidores)) %>% as.character()
-    paste("Número de Servidores:", t)
+    paste("   Número de Servidores: ", t)
     
   })
   
   #####################################CARREIRAS##########################################  
   
   REMUN_TOTAL = sum(servidores$REMUNERACAO_BRUTA)
-  CARGOS = enc2utf8(servidores %>% select(DESCRICAO_CARGO) %>% distinct() %>% arrange(desc(DESCRICAO_CARGO)) %>% as.matrix() %>% c())
+  CARGOS = enc2utf8(servidores %>% select(DESCRICAO_CARGO) %>% distinct() %>% arrange(DESCRICAO_CARGO) %>% as.matrix() %>% c())
   
   # Sidepanel
   output$carr1 <- renderUI({
@@ -91,7 +105,7 @@ shinyServer( function(input, output, session) {
   # TAB Todas
   output$carreiras <- DT::renderDataTable({
     datatable(servidores %>% group_by(DESCRICAO_CARGO) %>% summarise(Salario_Medio = round(mean(REMUNERACAO_BRUTA, rm.na=TRUE), 2), N_Servidores = n(), Gasto_Total=sum(REMUNERACAO_BRUTA), Percentual = round((Gasto_Total*100/REMUN_TOTAL),2)) %>% 
-      arrange(desc(Salario_Medio))) 
+                arrange(desc(Salario_Medio)), filter = "top", rownames = FALSE) 
   })
   
   
@@ -102,7 +116,7 @@ shinyServer( function(input, output, session) {
     if(is.null(input$carr2))
       return()
     SALARIO_CAR = servidores %>% filter(DESCRICAO_CARGO==input$carr2) %>% group_by(DESCRICAO_CARGO) %>%
-                  summarise(Numero_Servidores = n(), Salario_Medio = round(mean(REMUNERACAO_BRUTA), 2), Salario_Maximo = max(REMUNERACAO_BRUTA), Salario_Minimo = min(REMUNERACAO_BRUTA))
+      summarise(Numero_Servidores = n()*length(input$carr2), Salario_Medio = round(mean(REMUNERACAO_BRUTA), 2), Salario_Maximo = max(REMUNERACAO_BRUTA), Salario_Minimo = min(REMUNERACAO_BRUTA))
     datatable(SALARIO_CAR)
   })
   
@@ -111,8 +125,10 @@ shinyServer( function(input, output, session) {
       return()
     servidores %>% filter(DESCRICAO_CARGO %in% input$carr2) %>% 
       ggplot(aes(REMUNERACAO_BRUTA, fill = DESCRICAO_CARGO)) + 
-      geom_histogram(alpha=.8, position="dodge") + 
-      labs(x="Remuneração", y="", title="Distribuição") 
+      geom_histogram(alpha=.8, position="dodge", colour = "black") + 
+      labs( x="", y="", title="Distribuição") +
+      theme_economist() + scale_colour_economist() +
+      theme(plot.title = element_text(size=15, face="bold", vjust=2), legend.title=element_blank())
   })
   
   output$plot_comp2 <- renderPlot({
@@ -121,7 +137,9 @@ shinyServer( function(input, output, session) {
     servidores %>% filter(DESCRICAO_CARGO %in% input$carr2) %>% 
       ggplot(aes(REMUNERACAO_BRUTA, fill = DESCRICAO_CARGO)) + 
       geom_density(alpha=0.2) + 
-      labs(x="Remuneração", y="", title="Distribuição") 
+      labs(x="", y="", title="Distribuição") +
+      theme_economist() + scale_colour_economist() +
+      theme(plot.title = element_text(size=15, face="bold", vjust=2), legend.title=element_blank())
   })
   
   output$plot_comp3 <- renderPlot({
@@ -130,16 +148,20 @@ shinyServer( function(input, output, session) {
     servidores %>% filter(DESCRICAO_CARGO %in% input$carr2) %>% group_by(DESCRICAO_CARGO, UF_EXERCICIO) %>% summarise(TOTAL=n()) %>%
       ggplot(aes(UF_EXERCICIO, TOTAL, fill = DESCRICAO_CARGO)) + 
       geom_bar(stat = "identity", alpha=.8, position="dodge") +
-      labs(x="Remuneração", y="", title="Servidores por Estado") 
+      labs(x="", y="", title="Servidores por Estado") +
+      theme_economist() + scale_colour_economist() +
+      theme(plot.title = element_text(size=15, face="bold", vjust=2), legend.title=element_blank())
   })
   
   output$plot_comp4 <- renderPlot({
     if(is.null(input$carr2))
       return()
     servidores %>% filter(DESCRICAO_CARGO %in% input$carr2) %>% group_by(UF_EXERCICIO) %>%
-              ggplot(aes(UF_EXERCICIO, REMUNERACAO_BRUTA, fill = DESCRICAO_CARGO)) + 
-              geom_boxplot(colour = "darkblue") + 
-              labs(x="Estado", y="Quantidade de Servidores", title="Boxplot")
+      ggplot(aes(UF_EXERCICIO, REMUNERACAO_BRUTA, fill = DESCRICAO_CARGO)) + 
+      geom_boxplot(colour = "darkblue") + 
+      labs(x="", y="Quantidade de Servidores", title="Boxplot") +
+      theme_economist() + scale_colour_economist() +
+      theme(plot.title = element_text(size=15, face="bold", vjust=2), legend.title=element_blank())
   })
   
   output$plot_comp5 <- renderPlot({
@@ -149,6 +171,8 @@ shinyServer( function(input, output, session) {
       ggplot(aes(ORG_EXERCICIO, N, fill = DESCRICAO_CARGO)) + 
       geom_bar(colour = "darkblue", stat = "identity") + 
       labs(y="Quantidade de Servidores", title="Por Órgão") +
+      theme_economist() + scale_colour_economist() +
+      theme(plot.title = element_text(size=15, face="bold", vjust=2), legend.title=element_blank()) +
       coord_flip()
   })
   
@@ -157,7 +181,7 @@ shinyServer( function(input, output, session) {
   name = reactive({input$datatab_servidor_rows_current})
   nome = reactive({
     if(length(name()) == 1) servidores %>% slice(as.integer(name())) %>% select(NOME) %>% as.matrix() %>% c() else 1
-    })
+  })
   
   output$texto = renderText({paste("Nome: ", nome())})
   
@@ -169,21 +193,59 @@ shinyServer( function(input, output, session) {
     num_serv_acima = servidores %>% filter(DESCRICAO_CARGO == serv_cargo[1,] & REMUNERACAO_BRUTA > serv_remuneracao[1,]) %>% summarise(N_Servidores_Acima = n())
     num_serv_abaixo = servidores %>% filter(DESCRICAO_CARGO == serv_cargo[1,] & REMUNERACAO_BRUTA < serv_remuneracao[1,]) %>% summarise(N_Servidores_Abaixo = n())
     resumo_geral = cbind(num_serv_carg, num_serv_acima, num_serv_abaixo, remun_cargo)
-    datatable(resumo_geral) 
+    datatable(resumo_geral, options = list(dom = 't')) 
   })
   
   
   output$datatab_servidor <- DT::renderDataTable({
-    a = servidores %>% select(NOME, DESCRICAO_CARGO, FUNCAO, ORG_EXERCICIO, UF_EXERCICIO, SITUACAO_VINCULO, REMUNERACAO_BRUTA, VERBAS, JETONS) 
+    a = servidores %>% select(NOME, DESCRICAO_CARGO, FUNCAO, SITUACAO_VINCULO, ORG_EXERCICIO, UF_EXERCICIO, REMUNERACAO_BRUTA, VERBAS, JETONS) 
     action = dataTableAjax(session, a)
-    datatable(a, filter = "top", options = list(ajax = list(url = action)))
+    datatable(a, filter = "top", rownames = FALSE, options = list(ajax = list(url = action), autoWidth = TRUE 
+                                                # columnDefs = list(list(width = '300px', targets = c(2,3,4,5)))
+                                                ))
   })
   
   
   
   #############################################################################   
-#   servidores %>% group_by(ORG_EXERCICIO) %>% summarise(SM = mean(REMUNERACAO_BRUTA)) %>% arrange (desc(SM)) %>% top_n(20)
-#   servidores %>% filter(DESCRICAO_CARGO %in% input$carr2) %>% group_by(ORG_EXERCICIO) %>% summarise(N=n()) %>% arrange(desc(N))
+  ORGAOS = enc2utf8(servidores %>% select(ORG_EXERCICIO) %>% distinct() %>% arrange(ORG_EXERCICIO) %>% as.matrix() %>% c())
+  
+  output$orgaos1 <- renderUI({
+    selectizeInput("orgaos1", "Órgãos:", ORGAOS, selected = NULL, multiple = TRUE,
+                   options = NULL)
+  })
+ 
+  output$plot_org1 <- renderPlot({
+    if(is.null(input$orgaos1))
+      return()
+    servidores %>% filter(ORG_EXERCICIO %in% input$orgaos1) %>% group_by(ORG_EXERCICIO, DESCRICAO_CARGO) %>% summarise(N_Servidores=n()) %>% top_n(5) %>%
+      ggplot(aes(DESCRICAO_CARGO, N_Servidores)) + 
+      facet_grid(ORG_EXERCICIO ~.) +
+      geom_bar(stat = "identity", position = "dodge", fill = "darkblue") + 
+      labs(y="", x="", title="Número de Servidores por Órgão") +
+      theme(axis.title.x = element_text(face="bold", colour="#990000", size=12),
+            axis.text.x  = element_text(angle=90, vjust=0.5, size=12)) + 
+      coord_flip()
+#       theme(plot.title = element_text(size=15, face="bold", vjust=2), legend.title=element_blank()) +
+#       coord_flip()
+  })
+  
+  
+   
+#   output$plot_org1 <- renderPlot({
+#     if(is.null(input$orgaos1))
+#       return()
+#     servidores %>% filter(ORG_EXERCICIO %in% input$orgaos1) %>% group_by(ORG_EXERCICIO) %>% summarise(N_Servidores=n(), Custo=sum(REMUNERACAO_BRUTA)) %>% arrange(desc(N_Servidores)) %>%
+#       ggplot(aes(ORG_EXERCICIO, N_Servidores, fill = Custo)) + 
+#       geom_bar(stat = "identity", position = "dodge") + 
+#       labs(y="", x="", title="Número de Servidores por Órgão") +
+#       theme(plot.title = element_text(size=15, face="bold", vjust=2), legend.title=element_blank()) +
+#       coord_flip()
+#   })
 #   
+  
+  #   servidores %>% group_by(ORG_EXERCICIO) %>% summarise(SM = mean(REMUNERACAO_BRUTA)) %>% arrange (desc(SM)) %>% top_n(20)
+  #   servidores %>% filter(DESCRICAO_CARGO %in% input$carr2) %>% group_by(ORG_EXERCICIO) %>% summarise(N=n()) %>% arrange(desc(N))
+  #   
   
 })
--- ui.R
+++ ui.R
@@ -3,91 +3,108 @@ library(dplyr)
 library(data.table)
 library(DT)
 library(ggplot2)
+library(ggthemes)
 
 
-shinyUI(fluidPage(
+# Application title
 
-  # Application title
-  
-  shinyUI(navbarPage(
-    title = 'Servidores Federais',
-    tabPanel('Sumario',
-        wellPanel( 
-         fluidRow(
+shinyUI(navbarPage(
+  title = 'Servidores Federais',
+  tabPanel('Sumario',
+           wellPanel( 
+             fluidRow(
                column(4,
-                      sliderInput ("min_servidores", label = "Número mínimo de Servidores", 1, 2000, 1, step = 10)
+                       sliderInput ("min_servidores", label = "Número mínimo de Servidores", 0, 2000, 0, step = 10)
+                      # numericInput("min_servidores", label = "Número mínimo de Servidores", value=0, min=0, max=2000, step=10)
+                      
                ),
                
                column(4,
-                      sliderInput ("min_remuneracao", label = "Remuneração Mínima:", 1, 50000, 1, step = 1000)
+                       sliderInput ("min_remuneracao", label = "Remuneração Média Mínima:", 0, 50000, 0, step = 1000)
+                      # numericInput("min_remuneracao", label = "Remuneração Média Mínima:", value=0, min=0, max=50000, step= 1000)
                ),
                
                column(4,
-                      br(),
-                      div(style="color:darkblue", h4(textOutput("text1")), h4(textOutput("text2")))
+                      div(h4(textOutput("text1")), h4(textOutput("text2"))),
+                      actionButton("action1", "Atualizar Gráficos")
                ))
+           ),
+           
+           
+           fluidRow(
+             column(6,
+                    plotOutput('plot7')
              ),
-         
              
-          fluidRow(
-            column(6,
-              plotOutput('plot1')
-            ),
-            
-            column(6,
-              plotOutput("plot7")
-            )
-          ),
+             column(6,
+                    plotOutput("plot1")
+             )
+           ),
+           
+           fluidRow(
+             column(12,
+                    plotOutput('plot2')
+                    
+             )
+           )
+           
+  ),
+  
+  tabPanel('Carreira',
+           
+           
+           tabsetPanel(type = "tabs", 
+                       tabPanel("Todas",
+                                
+                                DT::dataTableOutput("carreiras")
+                                
+                       ), 
+                       
+                       tabPanel("Comparar",  
+                                uiOutput("carr2"),
+                                DT::dataTableOutput("carreiras3"),
+                                hr(),
+                                plotOutput('plot_comp1'),br(),
+                                plotOutput('plot_comp2'),br(),
+                                plotOutput('plot_comp3'),br(),
+                                plotOutput('plot_comp4'),br(),
+                                plotOutput('plot_comp5')
+                                
+                                
+                       )
+                       
+           )
+           
+           
+  ),
+  
+  tabPanel('Servidores',
+           
+           textOutput("texto"),
+           hr(),
+           DT::dataTableOutput("tab_resumo"),
+           hr(),
+           DT::dataTableOutput("datatab_servidor")
+           
+           
+           
+           
+  ),
+  
+  tabPanel('Órgãos',
+     tabsetPanel(type = "tabs",  
+           tabPanel("Comparar",
+              uiOutput("orgaos1"),
+              hr(),
+              plotOutput('plot_org1'),br()
+            ),    
+           tabPanel("Estatísticas",
+                    
+                    hr(),
+                    plotOutput(''),br()
+           )    
           
-          fluidRow(
-            column(12,
-                   plotOutput('plot2')
-                   
-            )
-          )
-          
-    ),
-    
-    tabPanel('Carreira',
-        
-          
-             tabsetPanel(type = "tabs", 
-                tabPanel("Todas",
-                      
-                      DT::dataTableOutput("carreiras")
-                      
-                ), 
-                
-                tabPanel("Comparar",  
-                         uiOutput("carr2"),
-                         DT::dataTableOutput("carreiras3"),
-                         plotOutput('plot_comp1'),
-                         plotOutput('plot_comp2'),
-                         plotOutput('plot_comp3'),
-                         plotOutput('plot_comp4'),
-                         plotOutput('plot_comp5')
-                         
-                         
-                )
-                
-            )
-          
-        
-    ),
-    
-    tabPanel('Servidores',
-             
-             textOutput("texto"),
-             hr(),
-             DT::dataTableOutput("tab_resumo"),
-             DT::dataTableOutput("datatab_servidor")
-             
-             
-             
-             
-    ),
-    
-    tabPanel('Futuro'       )
-    
-  ))
+        )
+     )
 ))
+
